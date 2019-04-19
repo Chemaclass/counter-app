64e7da62e67e91b95cdfafb3650fe078c17efd7c
Chemaclass <chemaclass@outlook.es>
2019-04-19 16:52:22 +0200

    Create method in Counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <h1>Hello World</h1>
-         );
-    }
+  state = {
+    count: 0
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
 }
- 
+
 export default Counter;
